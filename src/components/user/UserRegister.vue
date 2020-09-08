82fd2d628af29429df4f35d0febbf12bd2e9afa0
matthew <matthew@cupist.com>
2020-09-09 01:12:51 +0900

    [add] user-register 관련 테스트 추가

---
+++ src/components/user/UserRegister.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="UserRegister">
+    <div>
+      <input class="username" type="text" v-model="data.username">
+    </div>
+    <div>
+      <button class="register" type="submit" @click="register">등록</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name:"UserRegister",
+  data(){
+    return{
+      data:{
+        username:''
+      }
+    }
+  },
+  methods:{
+    register(){
+      return this.data
+    }
+  }
+}
+</script>
