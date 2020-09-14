76520272df78e1d8e2c321a912f66649253f0fbb
matthew <matthew@cupist.com>
2020-09-14 23:03:47 +0900

    [add] click-button 기본 모양 추가

---
+++ src/store/mutation-types.js
@@ -0,0 +1,2 @@
+export const GET_COUNTER = "GET_COUNTER";
+export const SET_COUNTER = "SET_COUNTER"
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div class="home"> Hello World </div>
+    <div> Counter : {{counter}} </div>
+    <button @click="increment">Press Me</button>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import {GET_COUNTER, SET_COUNTER} from "@/store/mutation-types";
+import {mapGetters, mapActions} from 'vuex'
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  name:"Home",
+  methods:{
+    ...mapActions({
+      increment:SET_COUNTER
+    })
+  },
+  computed:{
+    ...mapGetters({
+      counter:GET_COUNTER
+    })
   }
-};
+}
 </script>
