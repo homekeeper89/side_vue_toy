f11f46338519cd3d9a362430d3c1e8b5bc119e8b
matthew <matthew@cupist.com>
2020-09-17 01:12:39 +0900

    [add] vuex + moudle 관련 테스트 추가중
    vuex를 mock으로 테스트에서 만들지 않고 존재하는 코드를
    활용하는 방식 연구중

--- tests/unit/user/button.spec.js
+++ tests/unit/user/button.spec.js
@@ -1,15 +1,26 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Button from '@/components/button/ButtonClick.vue';
 import { customCounter as counter } from '@/store/button/counter';
+import Vuex from 'vuex';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
 describe('Button 컴포넌트', () => {
+  const store = new Vuex.Store(counter);
   it('렌더가 제대로 되는가, snapshot', () => {
-    const wp = shallowMount(Button);
+    const wp = shallowMount(Button, {
+      store,
+      localVue,
+    });
     expect(wp.html()).toMatchSnapshot();
   });
 
   it('버튼이 제대로 있는가', () => {
-    const wp = shallowMount(Button);
+    const wp = shallowMount(Button, {
+      store,
+      localVue,
+    });
     const btn = wp.find('.btn');
     const letter = 'Click ME';
     expect(btn.exists()).toBe(true);
@@ -17,8 +28,37 @@ describe('Button 컴포넌트', () => {
   });
 
   it('숫자가 나오는 곳이 있는가', () => {
-    const wp = shallowMount(Button);
+    const mockCounter = jest.fn(() => 7);
+    // const store = new Vuex.Store({
+    //   getters: counter.getters,
+    //   // getters: {
+    //   //   // mock function
+    //   //   'CUSTOM/GET_COUNTER': mockCounter,
+    //   // },
+    // });
+    const store = new Vuex.Store({
+      modules: {
+        counter,
+      },
+    });
+    const wp = shallowMount(Button, {
+      store,
+      localVue,
+    });
     const counterDiv = wp.find('.counter');
+    expect(store.state.counter).toBe(7);
+    // expect(counterDiv.text()).toBe('7');
+  });
+
+  it.skip('숫자가 나오는 곳이 있는가', () => {
+    const wp = shallowMount(Button, {
+      mocks: {
+        $store: store,
+      },
+      localVue,
+    });
+    const counterDiv = wp.find('.counter');
+    expect(store.state.counter).toBe(7);
     expect(counterDiv.text()).toBe('0');
   });
 
