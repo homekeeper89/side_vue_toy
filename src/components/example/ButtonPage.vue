8654e8b2f7e44ee609595b9a4354871700b45e5d
matthew <matthew@cupist.com>
2020-09-19 22:31:33 +0900

    [add] example 추가
    기초 테스트 및 컴포넌트 추가

---
+++ src/components/example/ButtonPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="count-show">
+      <span class="count-area">{{ counter }}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+};
+</script>
---
+++ tests/unit/example/button.spec.js
@@ -0,0 +1,16 @@
+// tests/unit/example/button.spec.js
+import { shallowMount } from '@vue/test-utils';
+import ButtonPage from '@/components/example/ButtonPage';
+
+describe('ButtonClick', () => {
+  it('Page 스냅샷을 찍자', () => {
+    const wrapper = shallowMount(ButtonPage);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('기본 영역을 확인하자 : 숫자 나오는 부분', () => {
+    const wrapper = shallowMount(ButtonPage);
+    const count = wrapper.find('.count-area').text();
+    expect(count).toBe('0');
+  });
+});
