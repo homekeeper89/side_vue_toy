97acdc89466c77656f5dfea20081b70ed750b444
matthew <matthew@cupist.com>
2020-09-23 01:20:12 +0900

    [add] state 테스트 기본 뼈대 추가

---
+++ tests/unit/store/example/store.spec.js
@@ -0,0 +1,7 @@
+import { exampleStore as store } from '@/store/modules/example';
+
+describe('example store test', () => {
+  it('counter의 기본값은 0이 되어야한다', () => {
+    expect(store.counter).toEqual(0);
+  });
+});
