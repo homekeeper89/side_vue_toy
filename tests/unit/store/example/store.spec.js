91463e417c58e50cbf1074e58a4635ef2dab4d85
matthew <matthew@cupist.com>
2020-09-25 01:08:49 +0900

    [add] 테스트 리팩토링
    중복라인제거

--- tests/unit/store/example/store.spec.js
+++ tests/unit/store/example/store.spec.js
@@ -1,21 +1,21 @@
 import { exampleStore as store } from '@/store/modules/example';
 
 describe('example store test', () => {
+  let state;
+  beforeEach(() => {
+    state = {
+      counter: 0,
+    };
+  });
   it('counter의 기본값은 0이 되어야한다', () => {
     expect(store.state.counter).toEqual(0);
   });
 
   it('getters는 state의 counter를 가져온다', () => {
-    const state = {
-      counter: 0,
-    };
     expect(store.getters.GET_COUNTER(state)).toEqual(state.counter);
   });
 
   it('counter의 값을 1 증가 시킨다', () => {
-    const state = {
-      counter: 0,
-    };
     store.mutations.SET_COUNTER(state);
     expect(state.counter).toEqual(1);
   });
