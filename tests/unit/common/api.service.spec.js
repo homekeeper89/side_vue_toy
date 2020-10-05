2259cc37540fc464a48332fbaba238876650b6a2
matthew <matthew@cupist.com>
2020-10-05 23:27:09 +0900

    [add] common 스텝 추가
    관련 기본 테스트 추가

---
+++ tests/unit/common/api.service.spec.js
@@ -0,0 +1,12 @@
+import { TagsService, ApiService } from '@/common/api.service';
+
+describe('API 관련 테스트', () => {
+  it('ApiService는 각 method가 존재해야한다', () => {
+    let methods = ['post', 'get', 'put', 'delete'];
+    let res = methods.filter(
+      (method) => !Object.keys(ApiService).includes(method)
+    );
+
+    expect(res.length).toEqual(0);
+  });
+});
